c1037dfa2b47fd003b69327ed5905d825861221a
daviddp98 <daviddp98@gmail.com>
2020-11-30 16:14:37 +0100

    v1

---
+++ androidStudio/FragmentList/app/src/main/java/com/example/fragmentlist/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.fragmentlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.contenedor, AveriaFragment.newInstance(3))
+                .commit();
+    }
+}
